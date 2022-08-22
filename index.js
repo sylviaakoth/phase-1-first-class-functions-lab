3c122be90c2a9cc956ad68fe75c1ba75253fcba6
Sylvia Akoth Omollo <sylvia_omollo@gwu.edu>
2022-08-22 15:30:04 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+//const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo']
+function returnFirstTwoDrivers(drivers) {
+    return drivers.slice(0, 2)
+}
+function returnLastTwoDrivers(drivers) {
+    return drivers.slice(2)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+const createFareMultiplier = function(num) {
+    return function(val) {
+        return val * num
+    }
+} 
+
+const fareDoubler = function(num) {
+    return num * 2
+}
+const fareTripler = function(num) {
+    return num * 3
+}
+
+const selectDifferentDrivers = function(drivers, callback) {
+    return callback(drivers)
+}
